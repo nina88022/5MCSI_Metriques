9d3da63affc939482f4a34635792c600b8d2feaa
Imane Bekkaoui <146001000+nina88022@users.noreply.github.com>
2024-11-20 15:41:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, rendertemplate, jsonify
+from flask import Flask, rendertemplatestring, rendertemplate, jsonify
 from flask import rendertemplate
 from flask import json
 from datetime import datetime
@@ -9,11 +9,23 @@ app = Flask(name)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #comm
+    return render_template('hello.html') #comm2
 
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
 
 if __name == "__main":
   app.run(debug=True)

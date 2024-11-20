407d384c0bb10546561259cc396b594bc05668c1
Imane Bekkaoui <146001000+nina88022@users.noreply.github.com>
2024-11-20 15:55:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, rendertemplatestring, render_template, jsonify
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
 from datetime import datetime
